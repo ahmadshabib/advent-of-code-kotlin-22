b1354b89a97efa3e4e85c3c6ef4f916fdf4aa4e7
Ahmad Shabib <ahmadshabib94@gmail.com>
2022-12-02 02:23:50 +0300

    first day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,20 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val elfs = mutableListOf<Long>()
+    var temp = 0L
+    input.forEach {
+        if (it.isNotEmpty()) {
+            temp += it.toLong()
+        } else {
+            elfs.add(temp)
+            temp = 0L
+        }
+    }
+    println(elfs)
+    // first part
+    println(elfs.max())
+    // second part
+    elfs.sortDescending()
+    println(elfs.subList(0,3).sum())
 }
